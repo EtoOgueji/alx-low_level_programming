b76755d2443d32af25bd12b274608fb0295cbe01
EtoOgueji <etothegoat65@gmail.com>
2022-12-09 15:59:49 -0800

    Prints all single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers
+ * start from 0
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= 9; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
