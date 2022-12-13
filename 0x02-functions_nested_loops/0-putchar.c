0bd587c442369706d455d927718bc00d62683d97
Etomchukwu Oguejiofor <etothegoat65@gmail.com>
2022-12-14 00:19:04 +0100

    prints 'putchar' followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - Prints _putchar
- * followed by a new line
- * Return: Always 0 (Success)
+ * main - Print _putchar
+ * Return: Always 0
  */
 int main(void)
 {
@@ -16,6 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
