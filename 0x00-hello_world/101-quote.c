c23624644435bec7ef981980dfbb6ed80233ccaf
EtoOgueji <etothegoat65@gmail.com>
2022-12-12 17:42:57 -0800

    prints out a character string

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a string
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	putchar(c);
+	putchar('\n');
+
+	return (0);
+}
