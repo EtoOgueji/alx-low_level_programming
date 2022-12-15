03d8c793c9fecdfb79436d875643ed7e50440cde
Etomchukwu Oguejiofor <etothegoat65@gmail.com>
2022-12-15 22:42:13 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,6 @@
 
 /**
 * print_numbers - print 0123456789
-*
 * Return: void
 */
 
@@ -11,7 +10,8 @@ void print_numbers(void)
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
-		_putchar(c);
-
+	{
+	_putchar(c);
+	}
 	_putchar('\n');
 }
