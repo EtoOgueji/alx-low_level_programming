fa80ac3b6d77e296e9703b8248268954799966ae
Etomchukwu Oguejiofor <etothegoat65@gmail.com>
2022-12-17 00:25:10 +0100

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,22 +5,24 @@
  * @size: size of the triangle
  * Return: VOid
  */
-print_triangle(int size)
-{
-	int rows, hashes, spaces;
 
-	if (size == 0)
+void print_triangle(int size)
+{
+	int row, hashes, spaces;
+
+	if (size <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
-
-		for (rows = 1; rows <= size; rows++)
+		for (row = 1; row <= size; row++)
 		{
-			for (spaces = size - rows; spaces >= 1; spaces--)
+			for (spaces = size - row; spaces >= 1; spaces--)
 			{
 				_putchar(' ');
 			}
-			for (hashes = 1; hashes <= rows; hashes++)
+			for (hashes = 1; hashes <= row; hashes++)
 			{
 				_putchar('#');
 			}
