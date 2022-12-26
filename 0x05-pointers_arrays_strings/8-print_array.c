2d7bcc95324cc15b4bb0424826072f113f10e4aa
EtoOgueji <etothegoat65@gmail.com>
2022-12-25 16:07:29 -0800

    prints the element of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,11 +14,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n - 1; i++)
+	for (; i < n; i++)
 	{
 		printf("%d", *(a + i));
-		_putchar(',');
-		_putchar(' ');
+		if (i != (n - 1))
+			printf(", ");
 	}
-	printf("%d", *(a + i));
+	printf("\n");
 }
