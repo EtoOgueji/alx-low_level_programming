7f8e2768d64e43837b8d733bb113b8e4c35972c2
EtoOgueji <etothegoat65@gmail.com>
2023-01-05 12:50:47 -0800

    prints a string recursively

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string to be printed
+ *
+ * Return: Void;
+ */
+
+void _puts_recursion(char *s)
+{
+	int i;
+
+	i = _strlen(s);
+
+	if (i == '\0')
+		return;
+	putchar(*s);
+
+	_puts_recursion(s + 1);
+}
