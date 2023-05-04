94d0b987c118c4d6dfc5b148907830cd48620bc2
Etomchukwu Oguejiofor <etoogueji@gmail.com>
2023-05-04 20:29:04 +0100

    sets a memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,13 +8,13 @@
  *
  * Return: S (Success)
  */
-char *_memset(char *s, char b, unsigned int n)
+char *_memset(char s[], char b, unsigned int n)
 {
 	int i = 0;
 
 	for (; n > 0; i++)
 	{
-		s[i] = b;
+		*(s + i) = b;
 		n--;
 	}
 	return (s);
